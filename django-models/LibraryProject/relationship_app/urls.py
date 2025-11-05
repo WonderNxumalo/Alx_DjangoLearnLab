3f42c8c714236e7cdd9b672cf1e0eb32628d28bb
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2025-11-05 23:29:02 +0200

    Auth

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -7,4 +7,7 @@ from . import views
 urlpatterns = [
     path('books/', views.list_all_books, name='book-list'),
     path('library/<int:pk>/', views.LibraryDefaultView.as_view(), name='library-detail'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.RegisterView.as_view(), name='register'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 # from django.views.generic.detail import DetailView
+# from django.contrib.auth import login
 from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic.edit import CreateView
 from .models import Book, Library
 
 # --- 1. Function-based View ---
@@ -11,14 +16,29 @@ def list_all_books(request):
     context = {'books': all_books}
     # Render list_books.html template
     # relationship_app/list_books.html
-    return render(request, 'list_books.html', context)
+    return render(request, 'relationship_app/list_books.html', context)
 
 # --- 2. Class-based View ---
 class LibraryDefaultView(DetailView):
     model = Library
     #relationship_app/library_detail.html ( from .models import Library )
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
     
     def get_queryset(self):
         return Library.objects.all().prefetch_related('books__author', 'librarian')
+    
+# --- 3. Authentication Views
+class CustomLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
+    # Optional: Define where to go after successful login
+    next_page = reverse_lazy('book-list')
+    
+class CustomLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+    next_page = reverse_lazy('login')
+    
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'relationship_app/register.html'
+    success_url = reverse_lazy('login')
