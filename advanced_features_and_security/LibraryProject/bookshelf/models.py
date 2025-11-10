edda39327dd91a071e231ec0c9df164d7975603b
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2025-11-10 10:57:37 +0200

    security

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -10,3 +10,28 @@ class Book(models.Model):
     
     class Meta:
         db_table = 'bookshelf_book'
+        
+'''
+# Custom User Model
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+    
+    objects = CustomUserManager()
+    
+    def __str__(self):
+        return self.username
+    
+    ROLE_ADMIN = 'Admin'
+    ROLE_LIBRARIAN = 'Librarian'
+    ROLE_MEMBER = 'Member'
+    
+    ROLE_CHOICES = [
+        (ROLE_ADMIN, 'Admin'),
+        (ROLE_LIBRARIAN, 'Librarian'),
+        (ROLE_MEMBER, 'Member'),
+    ]
+    
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default=ROLE_MEMBER)
+    
+'''
