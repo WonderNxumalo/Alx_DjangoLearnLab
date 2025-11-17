a68b29260f6966c3c2255b0b8303d8241f55f1e0
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2025-11-17 21:18:41 +0200

    Auth & Permissions

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     
 ]
 
+# rest_framework.permissions.IsAuthenticated
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
