25b5111dd4ab319603b21d9d1fba2bc13b8cf295
Wonder Nxumalo <144373619+WonderNxumalo@users.noreply.github.com>
2025-12-01 14:36:00 +0200

    Blog app Task 1

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -134,3 +134,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Errors (fixes)
+'''
+USER
+PORT
+'''
